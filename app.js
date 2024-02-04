ab8087a67b21974baf93fa55cfc48facb1d170cf
Elena Carey <theelenacarey@gmail.com>
2024-02-04 00:12:41 -0600

    Fixing swagger file.

--- app.js
+++ app.js
@@ -11,7 +11,14 @@ app
     .use(bodyParser.json())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, DELETE')
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept'
+        );
+        res.setHeader(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+        );
         next();
     })
     .use('/', require('./routes'));
